9e8fcf72f129a7a888830e1e8d6ac0fec88abcfa
wangding <1@q.q>
2021-03-09 15:44:38 +0800

    leetcode

--- src/main/java/cn/fenqing/arithmetic/leetcode/commons/Theme93.java
+++ src/main/java/cn/fenqing/arithmetic/leetcode/commons/Theme93.java
@@ -16,25 +16,18 @@ public class Theme93 {
 
     public void restoreIpAddresses(String s, int start, int level, List<String> res, String up) {
         if(level > 4){
-            if(up.length() == s.length() + 3){
-                res.add(up);
-            }
+            res.add(up);
             return;
         }
-        int end = start + 3;
+        int end = Math.min(start + 3, s.length());
         int num1 = (4 - level) * 3;
         for (int i = start; i < end; i++) {
             //每次取值后，计算剩余的字符够不够分
-            int num2 = s.length() - i;
+            int num2 = s.length() - i - 1;
             if(num1 < num2){
                 continue;
             }
-            String subString;
-            if(level == 4){
-                subString = s.substring(start);
-            }else{
-                subString = s.substring(start, i + 1);
-            }
+            String subString = s.substring(start, i + 1);;
             if (verify(subString)) {
                 if(level == 1){
                     restoreIpAddresses(s, i + 1, level + 1, res, subString);
@@ -49,15 +42,15 @@ public class Theme93 {
         if(s.isEmpty()){
             return false;
         }
-        if (s.charAt(0) == '0') {
+        if (s.length() > 1 && s.charAt(0) == '0') {
             return false;
         }
         int i = Integer.parseInt(s);
-        return i >= 1 && i <= 255;
+        return i >= 0 && i <= 255;
     }
 
     public static void main(String[] args) {
-        List<String> strings = new Theme93().restoreIpAddresses("25525511135");
+        List<String> strings = new Theme93().restoreIpAddresses("0000");
         for (String string : strings) {
             System.out.println(string);
         }
